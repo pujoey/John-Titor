ab4dee001b698acd67628bdf0a9197399b922e00
Joey Lin <pujoey@gmail.com>
2015-10-12 16:42:21 -0700

    finished dialog box

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
-
+// dialog script for John Titor character
+var gameCounter = 0; //counter used for game script
+var johnTitor = {
+  counter: 0,
+  script: [
+          "My Name is John Titor",
+          "I am a time traveler",
+          "I was just about to give up hope on anyone knowing who Tipler or Kerr was on this worldline.",
+          "The basics for time travel start at CERN in about a year and end in 2034 with the first 'time machine' built by GE.",
+          "Too bad you are not a scientist or I'd show it to you.",
+          "If you still want to know about time travel, then why don't you start by doing research online?",
+          "(Click on computer to start research)"
+          ]
+};
 
 // testing file loading
 console.log("***** MAIN.JS LOADED!!! ***")
 
-/***************** VARIABLE DECLARATION ***************/
+/***************** GAME RENDERING VARIABLE DECLARATION ***************/
 var gameArea = document.getElementById('gameArea'); //DOM div#gameArea
-var widthToHeight = 4 / 3;
+var widthToHeight = 10 / 6;
 var newWidth = window.innerWidth;
 var newHeight = window.innerHeight;
 var newWidthToHeight = newWidth / newHeight;
+
 
 
 
@@ -28,7 +42,7 @@ if (newWidthToHeight > widthToHeight) {
 gameArea.style.marginTop = (-newHeight / 2) + 'px'; 
 gameArea.style.marginLeft = (-newWidth / 2) + 'px';
 // adjust fontsize based on width
-gameArea.style.fontSize = (newWidth / 400) + 'em';
+gameArea.style.fontSize = (newWidth / 500) + 'em';
 
 var gameCanvas = document.getElementById('gameCanvas');
 gameCanvas.width = newWidth;
@@ -67,30 +81,45 @@ var ctx = gameCanvas.getContext("2d");
 
 
 /*************** MAIN MENU TESTING ************/
-function menu() {
-// ctx.font = newWidth / 16+ "px" + " Arial";
-// ctx.fillStyle = "#FFF";
-// ctx.fillText("Project 1: The Game",newWidth / 8, newHeight / 6);
+function script() {
 
 
-}
+    $( "#dialogBox" ).on( "click", function() {
+
+      if (gameCounter < 7) {
+        $("#dialogBox" ).text(johnTitor.script[johnTitor.counter]);
+          johnTitor.counter++; gameCounter++;
+          console.log(gameCounter);
+        }// end of if gameCounter < 7
+
+      if (gameCounter === 7) {  $("#dialogBox").prop('disabled', true); }
 
 
-// Background image
+    }); // end of dialogbox
+
+
+
+
+
+
+} //end of script
+
+
+/***************** ASSETS LOADING *****************/
 var bgReady = false;
 var bgImage = new Image();
 bgImage.onload = function () {
   bgReady = true;
 };
-bgImage.src = "images/background.png";
+bgImage.src = "images/background/bg03a4.jpg";
 
-// Hero image
-var heroReady = false;
-var heroImage = new Image();
-heroImage.onload = function () {
-  heroReady = true;
+// john titor image
+var johnReady = false;
+var johnImage = new Image();
+johnImage.onload = function () {
+  johnReady = true;
 };
-heroImage.src = "images/hero.png";
+johnImage.src = "images/faces.png";
 
 // Monster image
 var monsterReady = false;
@@ -102,7 +131,7 @@ monsterImage.src = "images/monster.png";
 
 // Game objects
 var hero = {
-  speed: 256 // movement in pixels per second
+  speed: 50 // movement in pixels per second
 };
 var monster = {};
 var monstersCaught = 0;
@@ -158,23 +187,28 @@ var update = function (modifier) {
 // Draw everything
 var render = function () {
   if (bgReady) {
-    ctx.drawImage(bgImage, 0, 0);
+    //ctx.drawImage(img, 0, 0, img.width,    img.height,    // source rectangle
+                   // 0, 0, canvas.width, canvas.height  // destination rectangle
+    ctx.drawImage(bgImage, 0, 0, 1024, 576, 0, 0, newWidth, newHeight);
   }
 
-  if (heroReady) {
-    ctx.drawImage(heroImage, hero.x, hero.y);
+  if (johnReady) {
+    //sx, sy, sw, sh, dx, dy, dw, dh
+    // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
+    ctx.drawImage(johnImage, 10, 0, 100, 121, 0, newHeight-242, 200, 242);
+
   }
 
-  if (monsterReady) {
-    ctx.drawImage(monsterImage, monster.x, monster.y);
-  }
+  // if (compReady) {
+  //   ctx.drawImage(compImage, );
+  // }
 
   // Score
   ctx.fillStyle = "rgb(250, 250, 250)";
   ctx.font = "24px Helvetica";
   ctx.textAlign = "left";
   ctx.textBaseline = "top";
-  ctx.fillText("Goblins caught: " + monstersCaught, 32, 32);
+  ctx.fillText("Research Point: " + monstersCaught, 32, 32);
 };
 
 // The main game loop
@@ -198,5 +232,5 @@ var main = function () {
 // Let's play this game!
 var then = Date.now();
 reset();
-// menu();
+script();
 main();
